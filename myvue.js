351b7c2ee5d14ed25937ceefc7ba443ebf8971d4
Ij888 <ij.ofili@gmail.com>
2017-09-08 14:22:03 +0100

    added entry
    input string updates the text

--- myvue.js
+++ myvue.js
@@ -2,5 +2,11 @@ new Vue({
     el: "#app",
     data: {
         blakka: "Pluto is no more a planet!"
-    }
+    },
+	methods:{
+		changeTitle: function(event){
+			this.blakka = event.target.value;
+		}
+	}
+	
 });
